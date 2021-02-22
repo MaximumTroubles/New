86f1c28ccfa3ea6323d7dbde94b365856f011f9b
Maxym <Rodziyevsky2@gmail.com>
2021-02-22 21:45:36 +0200

    Laravel-Home-Work-6.1 Added

---
+++ database/migrations/2021_02_22_184019_create_orders_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->integer('totalSum');
+            $table->string('name');
+            $table->string('phone');
+            $table->string('adress');
+            $table->unsignedBigInteger('status_id')->nullable();
+            $table->timestamps();
+            $table->foreign('status_id')->references('id')->on('statuses')->onDelete('set null');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
